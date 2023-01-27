880cb4b36a71835eecc4c011e2ee9de429a3634f
creaxcode01l <creaxcode01@outlook.fr>
2023-01-27 18:56:02 +0100

    1 files changed

--- collections.kt
+++ collections.kt
@@ -9,6 +9,10 @@
       "Chicken"
    )
 
+   val flavorList = listOf("Chocolate","Mango","Raspberry")
+
+   val birdsList = listOf("Eagle","Hawk","Turkey")
+
    println("the list is $shoppingList")
    println(shoppingList.size) // 3
  }
